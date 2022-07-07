54992cd9df25e4e074fd95aaf3bacd5fdaedd111
ephanzen@gmail.com <ephanzen4@gmail.com>
2022-07-07 22:16:32 +0700

    History Page

--- lib/Screens/Homepage/Page/history.dart
+++ lib/Screens/Homepage/Page/history.dart
@@ -1,5 +1,6 @@
 import 'package:capstone_project/Components/error_page.dart';
 import 'package:capstone_project/Components/loading_animation.dart';
+import 'package:capstone_project/Components/scroll_behavior.dart';
 import 'package:capstone_project/Components/text_style.dart';
 import 'package:capstone_project/State/enum.dart';
 import 'package:capstone_project/State/history_provider.dart';
@@ -33,38 +34,41 @@ class _RiwayatState extends State<Riwayat> {
         if (state.stateType == StateType.error) {
           return const ErrorPage();
         }
-        return ListView.separated(
-          itemBuilder: (ctx, i) {
-            return ListTile(
-              leading: state.data[i].productType! == "pulsa"
-                  ? Image.asset("assets/icons/icon-pulsa.png")
-                  : state.data[i].productType! == "quota"
-                      ? Image.asset("assets/icons/paket-data.png")
-                      : Image.asset("assets/icons/plus.png"),
-              title: HomeTextStyle(
-                size: 16,
-                content: state.data[i].productType!.toUpperCase(),
-                color: Colors.black,
-              ),
-              subtitle: HomeTextStyle(
-                size: 12,
-                content: state.data[i].dateString!,
-                color: Colors.black,
-              ),
-              trailing: HomeTextStyle(
-                size: 16,
-                content: state.data[i].productType! == "pulsa" ||
-                        state.data[i].productType! == "quota"
-                    ? "- Rp ${state.data[i].grossAmount!.toString()}"
-                    : "+ Rp ${state.data[i].grossAmount!.toString()}",
-                color: Colors.black,
-              ),
-            );
-          },
-          separatorBuilder: (ctx, i) {
-            return const Divider();
-          },
-          itemCount: state.data.length,
+        return ScrollConfiguration(
+          behavior: MyBehavior(),
+          child: ListView.separated(
+            itemBuilder: (ctx, i) {
+              return ListTile(
+                leading: state.data[i].productType! == "pulsa"
+                    ? Image.asset("assets/icons/icon-pulsa.png")
+                    : state.data[i].productType! == "quota"
+                        ? Image.asset("assets/icons/paket-data.png")
+                        : Image.asset("assets/icons/plus.png"),
+                title: HomeTextStyle(
+                  size: 16,
+                  content: state.data[i].productType!.toUpperCase(),
+                  color: Colors.black,
+                ),
+                subtitle: HomeTextStyle(
+                  size: 12,
+                  content: state.data[i].dateString!,
+                  color: Colors.black,
+                ),
+                trailing: HomeTextStyle(
+                  size: 16,
+                  content: state.data[i].productType! == "pulsa" ||
+                          state.data[i].productType! == "quota"
+                      ? "- Rp ${state.data[i].grossAmount!.toString()}"
+                      : "+ Rp ${state.data[i].grossAmount!.toString()}",
+                  color: Colors.black,
+                ),
+              );
+            },
+            separatorBuilder: (ctx, i) {
+              return const Divider();
+            },
+            itemCount: state.data.length,
+          ),
         );
       },
     );
