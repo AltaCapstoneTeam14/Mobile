c42e1b829b84e14bfd55e9ecdf9fedb9bcede77f
ephanzen@gmail.com <ephanzen4@gmail.com>
2022-07-08 10:12:04 +0700

    Fix History Page and Solve Bug

---
+++ lib/Components/background_primary.dart
@@ -0,0 +1,47 @@
+import 'package:capstone_project/Components/appbar_secondary.dart';
+import 'package:flutter/material.dart';
+
+class BackgroundPrimary extends StatelessWidget {
+  final String appBar;
+  final Widget child;
+  const BackgroundPrimary({
+    Key? key,
+    required this.child,
+    required this.appBar,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppbarSecondary(content: appBar),
+      body: SizedBox(
+        width: double.infinity,
+        height: size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              child: Container(
+                width: size.width,
+                height: size.height * 0.3,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/bg-primary.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: size.height * 0.37,
+              child: child,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Screens/Homepage/Page/history.dart
+++ lib/Screens/Homepage/Page/history.dart
@@ -2,6 +2,7 @@ import 'package:capstone_project/Components/error_page.dart';
 import 'package:capstone_project/Components/loading_animation.dart';
 import 'package:capstone_project/Components/scroll_behavior.dart';
 import 'package:capstone_project/Components/text_style.dart';
+import 'package:capstone_project/Screens/Homepage/Page/components/detail_history.dart';
 import 'package:capstone_project/State/enum.dart';
 import 'package:capstone_project/State/history_provider.dart';
 import 'package:flutter/material.dart';
@@ -38,29 +39,47 @@ class _RiwayatState extends State<Riwayat> {
           behavior: MyBehavior(),
           child: ListView.separated(
             itemBuilder: (ctx, i) {
-              return ListTile(
-                leading: state.data[i].productType! == "pulsa"
-                    ? Image.asset("assets/icons/icon-pulsa.png")
-                    : state.data[i].productType! == "quota"
-                        ? Image.asset("assets/icons/paket-data.png")
-                        : Image.asset("assets/icons/plus.png"),
-                title: HomeTextStyle(
-                  size: 16,
-                  content: state.data[i].productType!.toUpperCase(),
-                  color: Colors.black,
-                ),
-                subtitle: HomeTextStyle(
-                  size: 12,
-                  content: state.data[i].dateString!,
-                  color: Colors.black,
-                ),
-                trailing: HomeTextStyle(
-                  size: 16,
-                  content: state.data[i].productType! == "pulsa" ||
-                          state.data[i].productType! == "quota"
-                      ? "- Rp ${state.data[i].grossAmount!.toString()}"
-                      : "+ Rp ${state.data[i].grossAmount!.toString()}",
-                  color: Colors.black,
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailHistoryPage(
+                        date: state.data[i].dateString!,
+                        orderId: state.data[i].orderId!,
+                        status: state.data[i].status!,
+                        productType: state.data[i].productType!,
+                        name: state.data[i].name!,
+                        grossAmount: state.data[i].grossAmount!.toString(),
+                        paymentMethod: state.data[i].transferMethod!,
+                      ),
+                    ),
+                  );
+                },
+                child: ListTile(
+                  leading: state.data[i].productType! == "pulsa"
+                      ? Image.asset("assets/icons/icon-pulsa.png")
+                      : state.data[i].productType! == "quota"
+                          ? Image.asset("assets/icons/paket-data.png")
+                          : Image.asset("assets/icons/plus.png"),
+                  title: HomeTextStyle(
+                    size: 16,
+                    content: state.data[i].productType!.toUpperCase(),
+                    color: Colors.black,
+                  ),
+                  subtitle: HomeTextStyle(
+                    size: 12,
+                    content: state.data[i].dateString!,
+                    color: Colors.black,
+                  ),
+                  trailing: HomeTextStyle(
+                    size: 16,
+                    content: state.data[i].productType! == "pulsa" ||
+                            state.data[i].productType! == "quota"
+                        ? "- Rp ${state.data[i].grossAmount!.toString()}"
+                        : "+ Rp ${state.data[i].grossAmount!.toString()}",
+                    color: Colors.black,
+                  ),
                 ),
               );
             },
